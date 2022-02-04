04dd17866c6200482b11228ec5408378b1450163
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2022-02-03 18:14:29 -0800

    added main simulation for game

--- simulate-games.py
+++ simulate-games.py
@@ -18,3 +18,31 @@ class Simulation:
             index = random.randrange(0, len(words))
             return words[index].lower()
             
+    def simulate_game(self):
+        self.game.set_word()
+        
+        guess = self.choose_initial_word()
+        self.game.receive_guess(guess)
+        corr = self.game.get_corrected_word()
+
+        while not self.game.finished:
+            print(f'Guess is {guess}')
+            print(f'Correction is {corr}')
+            
+            self.assisstant.set_correction(corr)
+            self.assisstant.set_anchor()
+            self.assisstant.set_incorrect()
+
+            self.assisstant.incorrect = self.assisstant.clean_input(self.assisstant.incorrect)
+            self.assisstant.clean_correction = self.assisstant.clean_input(self.assisstant.correction)
+
+            self.assisstant.guess_word()
+            idx = random.randrange(0, len(self.assisstant.possible_words))
+            guess = self.assisstant.possible_words[idx]
+            self.game.receive_guess(guess)
+
+            corr = self.game.get_corrected_word()
+            self.game.end_game(corr)
+
+
+        print(f'The word was {self.game.word}!')
